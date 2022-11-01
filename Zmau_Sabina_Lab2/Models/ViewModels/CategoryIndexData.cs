774ca55fcaf56f8b61f349ae0811c4f1fb4e6e60
Sabina Zmau <sabina.zmau@yahoo.com>
2022-11-01 23:04:37 +0200

    Lab4_task

---
+++ Zmau_Sabina_Lab2/Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace Zmau_Sabina_Lab2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<BookCategory> BookCategories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
--- Zmau_Sabina_Lab2/Pages/Categories/Index.cshtml.cs
+++ Zmau_Sabina_Lab2/Pages/Categories/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Policy;
 using Zmau_Sabina_Lab2.Models;
+using Zmau_Sabina_Lab2.Models.ViewModels;
 
 namespace Zmau_Sabina_Lab2.Pages.Categories
 {
@@ -15,12 +17,25 @@ namespace Zmau_Sabina_Lab2.Pages.Categories
 
         public IList<Category> Category { get; set; } = default!;
 
-        public async Task OnGetAsync()
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            if (_context.Category != null)
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+            .Include(i => i.BookCategories)
+            .ThenInclude(i => i.Book)
+            .ThenInclude(i => i.Author)
+            .OrderBy(i => i.CategoryName)
+            .ToListAsync();
+            if (id != null)
             {
-                Category = await _context.Category.ToListAsync();
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories.Where(i => i.ID == id.Value).Single();
+                CategoryData.BookCategories = category.BookCategories;
             }
+
         }
     }
 }
