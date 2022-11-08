a9e4d3449a2503cb109935cc1351f0360f4c80d6
Sabina Zmău <sabina.zmau.sz@gmail.com>
2022-11-08 18:45:20 +0200

    lab5

--- Zmau_Sabina_Lab2/Migrations/20221108153003_CreateDB.Designer.cs
+++ Zmau_Sabina_Lab2/Migrations/20221108153003_CreateDB.Designer.cs
@@ -12,8 +12,8 @@ using Zmau_Sabina_Lab2.Data;
 namespace Zmau_Sabina_Lab2.Migrations
 {
     [DbContext(typeof(Zmau_Sabina_Lab2Context))]
-    [Migration("20221101150401_CreateMyDB")]
-    partial class CreateMyDB
+    [Migration("20221108153003_CreateDB")]
+    partial class CreateDB
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -101,6 +101,32 @@ namespace Zmau_Sabina_Lab2.Migrations
                     b.ToTable("BookCategory");
                 });
 
+            modelBuilder.Entity("Zmau_Sabina_Lab2.Models.Borrowing", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("ID"), 1L, 1);
+
+                    b.Property<int?>("BookID")
+                        .HasColumnType("int");
+
+                    b.Property<int?>("MemberID")
+                        .HasColumnType("int");
+
+                    b.Property<DateTime>("ReturnDate")
+                        .HasColumnType("datetime2");
+
+                    b.HasKey("ID");
+
+                    b.HasIndex("BookID");
+
+                    b.HasIndex("MemberID");
+
+                    b.ToTable("Borrowing");
+                });
+
             modelBuilder.Entity("Zmau_Sabina_Lab2.Models.Category", b =>
                 {
                     b.Property<int>("ID")
@@ -116,6 +142,35 @@ namespace Zmau_Sabina_Lab2.Migrations
                     b.HasKey("ID");
 
                     b.ToTable("Category");
+                });
+
+            modelBuilder.Entity("Zmau_Sabina_Lab2.Models.Member", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("ID"), 1L, 1);
+
+                    b.Property<string>("Adress")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Email")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("FirstName")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("LastName")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Phone")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Member");
                 });
 
             modelBuilder.Entity("Zmau_Sabina_Lab2.Models.Publisher", b =>
@@ -169,6 +224,21 @@ namespace Zmau_Sabina_Lab2.Migrations
                     b.Navigation("Category");
                 });
 
+            modelBuilder.Entity("Zmau_Sabina_Lab2.Models.Borrowing", b =>
+                {
+                    b.HasOne("Zmau_Sabina_Lab2.Models.Book", "Book")
+                        .WithMany()
+                        .HasForeignKey("BookID");
+
+                    b.HasOne("Zmau_Sabina_Lab2.Models.Member", "Member")
+                        .WithMany("Borrowings")
+                        .HasForeignKey("MemberID");
+
+                    b.Navigation("Book");
+
+                    b.Navigation("Member");
+                });
+
             modelBuilder.Entity("Zmau_Sabina_Lab2.Models.Author", b =>
                 {
                     b.Navigation("Books");
@@ -182,6 +252,11 @@ namespace Zmau_Sabina_Lab2.Migrations
             modelBuilder.Entity("Zmau_Sabina_Lab2.Models.Category", b =>
                 {
                     b.Navigation("BookCategories");
+                });
+
+            modelBuilder.Entity("Zmau_Sabina_Lab2.Models.Member", b =>
+                {
+                    b.Navigation("Borrowings");
                 });
 
             modelBuilder.Entity("Zmau_Sabina_Lab2.Models.Publisher", b =>
--- Zmau_Sabina_Lab2/Program.cs
+++ Zmau_Sabina_Lab2/Program.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Zmau_Sabina_Lab2.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Zmau_Sabina_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Zmau_Sabina_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Zmau_Sabina_Lab2Context' not found.")));
+
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+
+options.UseSqlServer(builder.Configuration.GetConnectionString("Zmau_Sabina_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Zmau_Sabina_Lab2Context' not found.")));
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+options.SignIn.RequireConfirmedAccount = true)
+ .AddEntityFrameworkStores<LibraryIdentityContext>();
+
 
 var app = builder.Build();
 
@@ -21,6 +29,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication(); ;
 
 app.UseAuthorization();
 
